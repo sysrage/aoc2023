571e680ca05a29b16e66b028c15e152e569f96e4
Brian Bothwell <sysrage@gmail.com>
2023-12-02 00:04:58 -0500

    fix :(

--- day1.mjs
+++ day1.mjs
@@ -1,4 +1,17 @@
 import { readFile } from 'node:fs/promises';
+
+const numberify = s => {
+    const numbers = ['one','two','three','four','five','six','seven','eight','nine'];
+    const characters = s.split('');
+    for (let i = 0; i < numbers.length; i++) {
+        for (const { index } of s.matchAll(numbers[i])) {
+            characters[index] = i + 1;
+        }
+    }
+    return characters.join('');
+}
+
+
 const data = await readFile('./data/day1.txt', 'utf-8');
 const lines = data.split('\n').filter(l => Boolean(l));
 
@@ -17,6 +30,9 @@ for (const line of lines) {
 }
 console.log(`Part 1: ${total}`);
 
+
+// PART 2
+
 const numbers = [
     'one',
     'two',
@@ -31,17 +47,11 @@ const numbers = [
 ];
 
 let total2 = 0;
-for (let line of lines) {
-    console.log('line', line);
-    for (let i = 0; i < line.length; i++) {
-        for (const [index, number] of numbers.entries()) {
-            if (line[i].match(/[0-9]/)) continue;
-            if (line.substring(i, i + number.length) === number) {
-                line = line.replace(number, `${index + 1}`);
-            }
-        }
-    }
-    console.log('new', line);
+for (const originalLine of lines) {
+    // console.log('originalLine', originalLine);
+    const line = numberify(originalLine);
+    // console.log('new', line);
+
 
     const chars = line.split('');
     let first, last;
@@ -52,7 +62,36 @@ for (let line of lines) {
         }
     }
     const value = parseInt(`${first}${last}`);
-    console.log('value', value);
+    // console.log('value', value);
     total2 += value;
+
+    // let firstLine = originalLine;
+    // // for (let i = 0; i < firstLine.length; i++) {
+    // //     for (const [index, number] of numbers.entries()) {
+    // //         if (firstLine[i].match(/[0-9]/)) continue;
+    // //         if (firstLine.substring(i, i + number.length) === number) {
+    // //             firstLine = firstLine.replace(number, `${index + 1}`);
+    // //         }
+    // //     }
+    // // }
+    // // console.log('new1', firstLine);
+
+    // let lastLine = originalLine;
+    // for (let i = lastLine.length - 1; i >= 0; i--) {
+    //     if (!isNaN(lastLine[i])) continue;
+    //     for (const [index, number] of numbers.entries()) {
+    //         if (lastLine.substring(i - number.length + 1, i + 1) === number) {
+    //             lastLine = lastLine.replace(number, `${index + 1}`);
+    //             i = i - number.length;
+    //         }
+    //     }
+    // }
+    // console.log('new2', lastLine);
+
+    // const first = firstLine.match(/[0-9]/)[0];
+    // const last = [...lastLine].reverse().join('').match(/[0-9]/)[0];
+    // const value = parseInt(`${first}${last}`);
+    // console.log('value', value);
+    // total2 += value;
 }
 console.log(`Part 2: ${total2}`);
