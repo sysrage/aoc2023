204f63c6fc10b41c7b734b517e435ace0cdc84c7
Brian Bothwell <sysrage@gmail.com>
2023-12-04 10:51:31 -0500

    part 2

--- day4.mjs
+++ day4.mjs
@@ -1,6 +1,14 @@
 import { readFile } from 'node:fs/promises';
 
 const data = await readFile('./data/day4.txt', 'utf-8');
+// const data = `
+// Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+// Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+// Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+// Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+// Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+// Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+// `;
 
 const lines = data.split('\n').filter(l => Boolean(l));
 
@@ -35,10 +43,19 @@ for (const line of lines) {
 
 console.log(`Part 1: ${totalPoints}`);
 
-const cardCounts = Object.fromEntries(Object.keys(cards).map(c => [c, 1]));
-console.log('cards', cards);
-console.log('counts', cardCounts);
+const cardCounts = Object.fromEntries(Object.keys(cards).map(c => [parseInt(c), 1]));
+// console.log('cards', cards);
+// console.log('counts', cardCounts);
 
-for (const cardNum of Object.keys(cards)) {
-    console.log(cardNum);
+for (const cardNumStr of Object.keys(cards)) {
+    const card = cards[cardNumStr];
+    const cardNum = parseInt(cardNumStr);
+    const matches = card.matching.length;
+    const cardCount = cardCounts[cardNum];
+    for (let i = cardNum + 1; i < cardNum + 1 + matches; i++) {
+        cardCounts[i] += cardCount;
+    }
 }
+
+// console.log('counts', cardCounts);
+console.log(`Part 2: ${Object.values(cardCounts).reduce((acc, cur) => acc + cur, 0)}`);
